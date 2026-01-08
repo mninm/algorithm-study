75bc6fc8b2362ae44ec81c31f0f0511a02a27bfb
sermyu <kyhee2001@naver.com>
2026-01-08 23:20:52 +0900

    solve: programmers lv0 배열의 평균 값

---
+++ app/src/main/java/programmers/level0/p015_array_avg.kt
@@ -0,0 +1,10 @@
+package programmers.level0
+
+//정수 배열 numbers의 원소의 평균값을 return
+
+class p015_array_avg {
+    fun solution(numbers: IntArray): Double {
+
+        return numbers.average()
+    }
+}
