768cf3f71a093c3b1b24280e061038f0162db812
sermyu <kyhee2001@naver.com>
2026-01-08 23:27:21 +0900

    solve: programmers lv0 뒤집힌 문자열

---
+++ app/src/main/java/programmers/level0/p017_reversed_string.kt
@@ -0,0 +1,10 @@
+package programmers.level0
+
+//문자열 my_string을 거꾸로 뒤집은 문자열을 return
+
+class p017_reversed_string {
+    fun solution(my_string: String): String {
+
+        return my_string.reversed()
+    }
+}
