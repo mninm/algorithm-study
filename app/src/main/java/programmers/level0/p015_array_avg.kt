c5b75bc2e9777a2c8933123ff1c53a8767d1d017
sermyu <kyhee2001@naver.com>
2026-01-08 23:22:59 +0900

    solve: programmers lv0 배열의 평균 값 풀이 추가

--- app/src/main/java/programmers/level0/p015_array_avg.kt
+++ app/src/main/java/programmers/level0/p015_array_avg.kt
@@ -7,4 +7,14 @@ class p015_array_avg {
 
         return numbers.average()
     }
+
+//    fun solution(numbers: IntArray): Double {
+//        var sum = 0
+//
+//        for (i in numbers) {
+//            sum += i
+//        }
+//
+//        return sum.toDouble() / numbers.size
+//    }
 }
