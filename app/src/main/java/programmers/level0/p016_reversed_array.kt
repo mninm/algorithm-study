cccdb05d5e0e55c2149fb84722abfe881945ef98
sermyu <kyhee2001@naver.com>
2026-01-08 23:25:13 +0900

    solve: programmers lv0 배열 뒤집기

---
+++ app/src/main/java/programmers/level0/p016_reversed_array.kt
@@ -0,0 +1,10 @@
+package programmers.level0
+
+//정수가 들어 있는 배열 num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return
+
+class p016_reversed_array {
+    fun solution(num_list: IntArray): IntArray {
+
+        return num_list.reversedArray()
+    }
+}
