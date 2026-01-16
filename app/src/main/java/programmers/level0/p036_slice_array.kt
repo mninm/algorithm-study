1abee23feb391a89f217206d84f5ae36851b3837
sermyu <kyhee2001@naver.com>
2026-01-16 21:29:50 +0900

    docs: 문제 주석 추가

--- app/src/main/java/programmers/level0/p036_slice_array.kt
+++ app/src/main/java/programmers/level0/p036_slice_array.kt
@@ -1,5 +1,7 @@
 package programmers.level0
 
+//정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return
+
 class p036_slice_array {
     fun solution(numbers: IntArray, num1: Int, num2: Int): IntArray {
         var answer = mutableListOf<Int>()
