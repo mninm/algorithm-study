30fc87ff74eee77681cb0082801923683c597baf
sermyu <kyhee2001@naver.com>
2026-01-21 01:36:18 +0900

    solve: programmers lv0 약수 구하기

---
+++ app/src/main/java/programmers/level0/p053_divisor.kt
@@ -0,0 +1,15 @@
+package programmers.level0
+
+//정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return
+
+class p053_divisor {
+    fun solution(n: Int): IntArray {
+        var answer = mutableListOf<Int>()
+        for (i in 1..n) {
+            if (n % i == 0) {
+                answer.add(i)
+            }
+        }
+        return answer.toIntArray()
+    }
+}
