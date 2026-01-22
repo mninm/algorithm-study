598c8194886638ddb0d5fed342d91e49b67efbaf
sermyu <kyhee2001@naver.com>
2026-01-22 22:04:29 +0900

    solve: programmers lv0 모음 제거

---
+++ app/src/main/java/programmers/level0/p056_filter_aeiou.kt
@@ -0,0 +1,10 @@
+package programmers.level0
+
+//영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return
+
+class p056_filter_aeiou {
+    fun solution(my_string: String): String {
+
+        return my_string.filter { it !in "aeiou" }
+    }
+}
