cac8e1a75f13201491fd26898aac8f2e7483f955
sermyu <kyhee2001@naver.com>
2026-01-28 23:04:23 +0900

    solve: programmers lv0 저주의 숫자 3

--- app/src/main/java/programmers/level0/p074_3x.kt
+++ app/src/main/java/programmers/level0/p074_3x.kt
@@ -16,4 +16,18 @@ class p074_3x {
         }
         return answer
     }
+
+
+//    fun solution(n: Int): Int {
+//        var answer = 0
+//
+//        for (i in 0 until n) {
+//            answer++
+//            while (answer % 3 == 0 || answer.toString().contains("3")) {
+//                answer++
+//            }
+//        }
+//        return answer
+//    }
+
 }
